85ebf8ec9b4cd1ab68f08e49bcb794f0d38e82ea
irem <iremtopcu69@gmail.com>
2025-10-09 10:13:34 +0300

    init

--- StaticLifetime.java
+++ StaticLifetime.java
@@ -1,2 +1,24 @@
 public class StaticLifetime {
+ //Q1
+    static int counter=0;
+    public void incrementsCounter() {
+        counter++;
+        System.out.println("Counter: " + counter);
+    }
+    public static void main (String[]args){
+        StaticLifetime obj1= new StaticLifetime();
+        obj1.incrementsCounter();
+        StaticLifetime obj2= new StaticLifetime();
+        obj2.incrementsCounter();
+    }
+
+
+
+
+
+
 }
+
+
+
+
